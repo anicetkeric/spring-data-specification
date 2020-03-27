9a036e9ebfa1373b7f4cfb220eaca44c6d312166
Anicetkeric <anicetkeric@gmail.com>
2020-03-27 22:19:24 +0100

    add data script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO department (id, name) VALUES
+                        (1, 'IT'), (2, 'HR'),(3, 'RAD');
+
+
+
+INSERT INTO employee (id,first_name,last_name,department_id) VALUES
+                     (1,'John','Doe',1),
+                     (2,'Carine','Marsh',1),
+                     (3,'Bernard','Rocher',3),
+                     (4,'Sabrina','Aliams',2),
+                     (6,'Manto','Virginie',2),
+                     (7,'Ramesh','Samir',2);
